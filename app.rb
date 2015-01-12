a7d5e0d1a14d063431609e0d0f7e93b3a4a72481
Duane M. Moody <dmmoody@gmail.com>
2015-01-12 14:15:11 -0800

    Add package dimension for and shipping cost results page

--- app.rb
+++ app.rb
@@ -6,3 +6,13 @@ require('./lib/parcels')
 get('/') do
   erb(:form)
 end
+
+get('/result') do
+  @width = params.fetch("width").to_i
+  @length = params.fetch("length").to_i
+  @depth = params.fetch("depth").to_i
+  parcel = Parcels.new()
+  parcel.volume(@length, @width, @depth)
+  @ship_cost = parcel.cost_to_ship()
+  erb(:result)
+end
